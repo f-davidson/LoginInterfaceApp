e034caa7fd1d3045411c390281449058af1328cf
FranChell Davidson <franno@FranChells-MacBook-Pro.local>
2017-02-01 10:17:53 -0600

    Initial Commit

---
+++ LoginInterfaceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginInterfaceApp
+//
+//  Created by FranChell Davidson on 2/1/17.
+//  Copyright © 2017 FranChell Davidson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
